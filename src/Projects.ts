24f69b61a0d2889c3795c532259fb0f265bd89c8
Henok Belachew <henokbelachewstu@gmail.com>
2024-03-31 00:20:53 +0300

    project 2 removed

--- src/Projects.ts
+++ src/Projects.ts
@@ -14,18 +14,7 @@ export const Projects =
       "img": img1,
       "description": "Aesthetically pleasing and responsive social media notification page using React/TypeScript and Tailwind CSS."
     },
-    {
-      "projectName": "Notifications page",
-      "html": true,
-      "css": true,
-      "ts": true,
-      "api": false,
-      "tag": "junior",
-      "demoLink": "https://henok-belachew.github.io/Notifications-Page/",
-      "sourceCodeLink": "https://github.com/Henok-Belachew/My-Frontend-Projects/tree/master",
-      "img": img1,
-      "description": "Aesthetically pleasing and responsive social media notification page using React/TypeScript and Tailwind CSS."
-    },
+    
     
   ]
   
